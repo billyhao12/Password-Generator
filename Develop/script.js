c2c57e1410d35e4974aa8ed07ee1b1b3cb9472fe
Billy Hao <billyhao12@gmail.com>
2020-04-18 22:16:55 -0700

    Added an event listener that generates a password upon clicking the button

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,36 @@
-// Assignment Code
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
 var generateBtn = document.querySelector("#generate");
 
-var passLength = 0,
-  lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var passwordText = document.querySelector("#password");
 
-function isPassLengthCorrect(passLength) {
-  return passLength >= 8 && passLength <= 128;
+generateBtn.addEventListener("click", startNewPassword);
+
+function startNewPassword() {
+  var passLength = 0;
+
+  function isPassLengthCorrect(passLength) {
+    return passLength >= 8 && passLength <= 128;
+  }
+
+  while (!isPassLengthCorrect(passLength)) {
+    passLength = parseInt(prompt("Provide a password length between 8 and 128"));
+  }
+
+  var options = {
+    "passLength": passLength,
+    "lowercase": confirm("Include lowercase letters?"),
+    "uppercase": confirm("Include uppercase letters?"),
+    "numeric": confirm("Include numbers?"),
+    "specialCharacters": confirm("Include special characters?")
+  };
+
+  passwordText.value = generatePassword(options);
+
 }
-
-while (!isPassLengthCorrect(passLength)) {
-  passLength = parseInt(prompt("Provide a password length between 8 and 128"));
-}
-
-var options = {
-  "passLength": passLength,
-  "lowercase": confirm("Include lowercase letters?"),
-  "uppercase": confirm("Include uppercase letters?"),
-  "numeric": confirm("Include numbers?"),
-  "specialCharacters": confirm("Include special characters?")
-};
 
 function generatePassword(options) {
   var password = "";
@@ -46,26 +55,12 @@ function generatePassword(options) {
   }
 
   for (var i = 0; i < options.passLength; i++) {
-    if(requiredCharacters[i]) {
+    if (requiredCharacters[i]) {
       password += requiredCharacters[i];
     } else {
       password += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
     }
   }
-  
+
   return password;
 }
-
-generatePassword(options);
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
