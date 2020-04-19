b3e2454b8c693437bb4ea9e7c86a0f6229df3d80
Billy Hao <billyhao12@gmail.com>
2020-04-18 20:49:49 -0700

    Created a function that generates a random password

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var passLength = 0,
   lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 function isPassLengthCorrect(passLength) {
@@ -25,18 +25,38 @@ var options = {
 
 function generatePassword(options) {
   var password = "";
-    possibleCharacters = [];
-  
-  if(options.lowercase) {
+  possibleCharacters = [];
+  requiredCharacters = [];
+
+  if (options.lowercase) {
     possibleCharacters = possibleCharacters.concat(lowercase);
+    requiredCharacters.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
   }
-  if(options.uppercase) {
+  if (options.uppercase) {
     possibleCharacters = possibleCharacters.concat(uppercase);
+    requiredCharacters.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+  }
+  if (options.numeric) {
+    possibleCharacters = possibleCharacters.concat(numeric);
+    requiredCharacters.push(numeric[Math.floor(Math.random() * numeric.length)]);
+  }
+  if (options.specialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    requiredCharacters.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
   }
 
+  for (var i = 0; i < options.passLength; i++) {
+    if(requiredCharacters[i]) {
+      password += requiredCharacters[i];
+    } else {
+      password += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+    }
+  }
   
   return password;
 }
+
+generatePassword(options);
 
 // Write password to the #password input
 function writePassword() {
